3240af18fe8588e846f8ee6507c02c7a9c049f0e
GraceJonn123 <gj.gjonn@gmail.com>
2016-03-05 18:35:38 +0300

    fixin bugs in current apps

--- Qwiz/app/src/main/java/com/jijjy/grace/qwiz/MainActivity.java
+++ Qwiz/app/src/main/java/com/jijjy/grace/qwiz/MainActivity.java
@@ -22,46 +22,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button b1 = (Button)findViewById(R.id.button);
-        Button b2 = (Button)findViewById(R.id.button2);
-        Button b3 = (Button)findViewById(R.id.button3);
-        Button b4 = (Button)findViewById(R.id.button4);
-        final TextView t1 = (TextView)findViewById(R.Id.textview2);
+        Button b1 = (Button) findViewById(R.id.button);
+        Button b2 = (Button) findViewById(R.id.button2);
+        Button b3 = (Button) findViewById(R.id.button3);
+        Button b4 = (Button) findViewById(R.id.button4);
+        TextView t1 = (TextView) findViewById(R.id.textView2);
 
-        Button[] button = {b1,b2,b3,b4};
-        for (Button aButton : button) {
-            aButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    switch (v.getId()) {
-                        case R.id.button:
-                            Intent i = new Intent(getBaseContext(), Local.class);
-                            startActivity(i);
-                            break;
-                        case R.id.button2:
-                            Intent j = new Intent(getBaseContext(), Int.class);
-                            startActivity(j);
-                            break;
-                        case R.id.button:
-                            Intent i = new Intent(getBaseContext(), Local.class);
-                            startActivity(i);
-                            break;
-                        case R.id.button2:
-                            Intent j = new Intent(getBaseContext(), Int.class);
-                            startActivity(j);
-                            break;
-                        default:
-                            break;
+//
+////
+////        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+////        fab.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View view) {
+////                Snackbar.make(view, "What are you doing", Snackbar.LENGTH_LONG)
+////                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "What are you doing", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-}
+//}
+    }}
