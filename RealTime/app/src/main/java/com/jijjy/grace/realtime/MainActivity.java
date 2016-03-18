602b828258dc3b1fc2726e90ce827a999fc0c23f
GraceJonn123 <gj.gjonn@gmail.com>
2016-03-19 00:41:45 +0300

    Realtime app

---
+++ RealTime/app/src/main/java/com/jijjy/grace/realtime/MainActivity.java
@@ -0,0 +1,54 @@
+package com.jijjy.grace.realtime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private  static final String FIREBASE_URL = "https://realtyme.firebaseio.com/";
+
+    private Firebase firebaseRef;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        firebaseRef = new Firebase(FIREBASE_URL);
+
+        EditText inputText = (EditText)findViewById(R.id.messageText);
+        inputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND){
+                    sendMessage();
+                }
+                return true;
+            }
+    });
+        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+    }
+    public void  sendMessage(){
+        EditText textInput = (EditText) findViewById(R.id.messageText);
+        String message = textInput.getText().toString();
+        if(!message.equals("")){
+            Random rand = new Random();
+            String author = "Grace" + rand.nextInt(10);
+            ChatMessage cMessage = new ChatMessage(author, message);
+            firebaseRef.push().setValue(cMessage);
+            textInput.setText("");
+        }
+    }
+}
