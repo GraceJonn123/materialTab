941c55c5a496c52202f491318e9217efe3e6897f
GraceJonn123 <gj.gjonn@gmail.com>
2016-03-20 19:59:46 +0300

    added new work

--- Retrieve/app/src/main/java/com/jijjy/grace/retrieve/MainActivity.java
+++ Retrieve/app/src/main/java/com/jijjy/grace/retrieve/MainActivity.java
@@ -5,48 +5,62 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //firebase
+    Firebase nRootRef;
+    ArrayList<String> nMessages = new ArrayList<>();
+
+    //ui
+    EditText neditTextMessage;
+    Button nbuttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //usually done in an application class
+        Firebase.setAndroidContext(this);
+
+        nRootRef = new Firebase("https://retrevar.firebaseio.com/");
+
+        neditTextMessage = (EditText)findViewById(R.id.editTextMessage);
+        nbuttonSubmit = (Button)findViewById(R.id.buttonSubmit);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Firebase messagesRef = nRootRef.child("messages");
+        messagesRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String message = dataSnapshot.getValue(String.class);
+                Log.v("E_VALUE", message);
+                neditTextMessage.setText(message);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
